c35df3015c0e19ad1d81d38d602662ce8c39dc92
Gil <lancemaker@gmail.com>
2020-03-21 21:49:59 -0300

    ficando bonito

--- view/templates/header.php
+++ view/templates/header.php
@@ -1,59 +1,37 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container-xl">
-    <a class="navbar-brand" href="#">Container XL</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07XL" aria-controls="navbarsExample07XL" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+<header>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-xl">
+      <a class="navbar-brand" href="#">Container XL</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07XL" aria-controls="navbarsExample07XL" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+  
+      <div class="collapse navbar-collapse" id="navbarsExample07XL">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="dropdown07XL" data-toggle="dropdown" aria-expanded="false">Dropdown</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown07XL">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-md-0">
+          <input class="form-control" type="text" placeholder="Search" aria-label="Search">
+        </form>
+      </div>
+    </div>
+  </nav>
+</header>
 
-    <div class="collapse navbar-collapse" id="navbarsExample07XL">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="dropdown07XL" data-toggle="dropdown" aria-expanded="false">Dropdown</a>
-          <div class="dropdown-menu" aria-labelledby="dropdown07XL">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </div>
-        </li>
-      </ul>
-      <form class="form-inline my-2 my-md-0">
-        <input class="form-control" type="text" placeholder="Search" aria-label="Search">
-      </form>
-    </div>
-  </div>
-</nav>
 
-
-<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner ">
-    <div class="carousel-item active">
-      <div class="carrossel-card-bg" style="background-image: url('assets/thumbs/1917.jpg');"></div>
-      <img src="assets/thumbs/1917.jpg" class="carrossel-card d-block" alt="...">
-    </div>
-    <div class="carousel-item">
-      <div class="carrossel-card-bg" style="background-image: url('assets/thumbs/FordvFerrari.jpg');"></div>
-      <img src="assets/thumbs/FordvFerrari.jpg" class="carrossel-card d-block" alt="...">
-    </div>
-    <div class="carousel-item">
-      <div class="carrossel-card-bg" style="background-image: url('assets/thumbs/Parasite.jpg');"></div>
-      <img src="assets/thumbs/Parasite.jpg" class="carrossel-card d-block" alt="...">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
--- view/templates/main.php
+++ view/templates/main.php
@@ -1,1 +1,26 @@
-<h1>main content</h1>
+<article>
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner ">
+    <div class="carousel-item active">
+      <div class="carrossel-card-bg" style="background-image: url('assets/thumbs/1917.jpg');"></div>
+      <img src="assets/thumbs/1917.jpg" class="carrossel-card d-block" alt="...">
+    </div>
+    <div class="carousel-item">
+      <div class="carrossel-card-bg" style="background-image: url('assets/thumbs/FordvFerrari.jpg');"></div>
+      <img src="assets/thumbs/FordvFerrari.jpg" class="carrossel-card d-block" alt="...">
+    </div>
+    <div class="carousel-item">
+      <div class="carrossel-card-bg" style="background-image: url('assets/thumbs/Parasite.jpg');"></div>
+      <img src="assets/thumbs/Parasite.jpg" class="carrossel-card d-block" alt="...">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</article>
