b498e0160e21025379474ac39591d9c472b653fd
Gil <lancemaker@gmail.com>
2020-03-27 19:55:53 -0300

    form

--- view/templates/header.php
+++ view/templates/header.php
@@ -20,7 +20,7 @@
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="dropdown07XL" data-toggle="dropdown" aria-expanded="false">Dropdown</a>
             <div class="dropdown-menu" aria-labelledby="dropdown07XL">
-              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="view/templates/footer.php">Forms</a>
               <a class="dropdown-item" href="#">Another action</a>
               <a class="dropdown-item" href="#">Something else here</a>
             </div>
