f5ef3d765fb478f1581dc38064cf390b107ec11b
Gil <lancemaker@gmail.com>
2020-03-27 19:49:13 -0300

    form

--- view/templates/formularioCompra.php
+++ view/templates/formularioCompra.php
@@ -1,5 +1,5 @@
 <div style="width:40%;margin: 0 auto">
-<form action="" name="Form1" method="get">
+<form action="" name="Form1" method="POST">
 <p>nome:
 <input type="text" name="nome" size="20"/>
 <br>
@@ -9,11 +9,30 @@
 </div>
 
 <div style="width:40%;margin: 0 auto">
-<form action="" name="Form2" method="get">
+<form action="" name="Form2" method="POST">
 <p>nome:
 <input type="text" name="user" size="20"/>
 <br>
 <p>Pass:
 <input type="password" name="password" size="20/">
+</form>
+</div>
+
+<div style="width:40%;margin: 0 auto">
+<form action="" name="Form3" method="POST">
+
+<p>Eu tenho uma bike:
+<input type="checkbox" name="tem_bike" value="bike"/>
+<br>
+
+<p>Eu tenho um carro:
+<input type="checkbox" name="tem_bike" value="bike"/>
+<br>
+
+<p>Eu tenho uma avião:
+<input type="checkbox" name="tem_aviao" value="aviao"/>
+<br>
+
+
 </form>
 </div>
