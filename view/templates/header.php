237d28c870b8c05ad6ddbf3ff5c5521677825ebb
Gil <lancemaker@gmail.com>
2020-04-05 15:22:47 -0300

    test

--- view/templates/header.php
+++ view/templates/header.php
@@ -15,12 +15,11 @@
             <a class="nav-link" href="#">Link</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          <a class="dropdown-item" href="view/templates/formularioCompra.php">Cadastro</a>
           </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="dropdown07XL" data-toggle="dropdown" aria-expanded="false">Dropdown</a>
-            <div class="dropdown-menu" aria-labelledby="dropdown07XL">
-              <a class="dropdown-item" href="view/templates/formularioCompra.php">Forms</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown07XL">              
               <a class="dropdown-item" href="#">Another action</a>
               <a class="dropdown-item" href="#">Something else here</a>
             </div>
