4748d99a835fb0d34d32b606fe1775a3753547ee
Gil <lancemaker@gmail.com>
2020-04-06 20:34:20 -0300

    changes on css

--- view/templates/header.php
+++ view/templates/header.php
@@ -26,6 +26,7 @@
           <li class="nav-item">
             <a class="nav-link" href="index.php?page=CvLeo">Sobre Nós</a>
           </li>
+          
           <!-- <li class="nav-item">
             <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
           </li>
