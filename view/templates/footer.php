3825cd47c29bd4d2e6157d6798d05c2ae733534d
Rodrigosavone <rodrigosavonerbs@gmail.com>
2020-04-06 23:17:52 -0300

    footer

--- view/templates/footer.php
+++ view/templates/footer.php
@@ -1,30 +1,25 @@
 <div class="footer">
 
-        <!-- Content -->
-        <h5 align="center" font-size:16px>Cineminha</h5>
-        <p>Muito obrigado por entrar no Cineminhas</p>
-<<<<<<< HEAD
+  <!-- Content -->
+  <h5 align="center" font-size:16px>Cineminha</h5>
+  <p>Muito obrigado por entrar no Cineminhas</p>
 </div>
 <div class="footer">© 2020 Copyright:
   <a> Seiji,Rodrigo,LeoGil</a>
 </div>
-  <img src="assets/thumbs/wpp.png" alt="Smiley face" height="42" width="42">
-</footer>
-=======
-    
-    <img src="assets/thumbs/wpp.png" alt="Smiley face" height="24" width="24">
-    <img src="assets/thumbs/face.png" alt="Smiley face" height="24" width="24">
-    <br>
-    <a>© 2020 Copyright:<a>
-    <a> Seiji,Rodrigo,LeoGil</a>
-
+<div>
+  <img src="assets/thumbs/wpp.png" alt="Smiley face" height="24" width="24">
+  <img src="assets/thumbs/face.png" alt="Smiley face" height="24" width="24">
+  <br>
+  <a>© 2020 Copyright:<a>
+      <a> Seiji,Rodrigo,LeoGil</a>
 </div>
 
 </footer>
 
->>>>>>> f31b43890321890fe8970e72405cd023baf575b0
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
+
 </html>
