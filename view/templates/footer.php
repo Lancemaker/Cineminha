bd67f7d1913d8d832152313109d7209427abd6b4
LeonardoSa <33468255+LeoonardoSa@users.noreply.github.com>
2020-04-06 23:05:06 -0300

    Institucional

--- view/templates/footer.php
+++ view/templates/footer.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <div class="footer">
 
         <!-- Content -->
@@ -12,9 +11,6 @@
 
 
 </footer>
-=======
-<h1>footer</h1>
->>>>>>> 7f1dd0609b2ec1e62c503f365b26f3cda4796d34
 
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
