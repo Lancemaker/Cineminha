b2d665fbd65cde0f3519fd85c1f5ea298e803d6a
Gil <lancemaker@gmail.com>
2020-04-12 20:00:13 -0300

    curriculos

--- view/templates/sobrenos.php
+++ view/templates/sobrenos.php
@@ -2,8 +2,8 @@
     <div class="col-md-6">
         <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
             <div class="col p-4 d-flex flex-column position-static">
-                <strong class="d-inline-block mb-2 text-primary">World</strong>
-                <h3 class="mb-0">Featured post</h3>
+                <strong class="d-inline-block mb-2 text-primary">Gil Felipe da Silva</strong>
+                <h3 class="mb-0">Analista JR</h3>
                 <div class="mb-1 text-muted">Nov 12</div>
                 <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
                 <a href="#" class="stretched-link">Continue reading</a>
