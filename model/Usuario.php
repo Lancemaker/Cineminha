7fdcd16156ec1a4d49a90feeff5f57c00cfa4fe7
LeonardoSa <33468255+LeoonardoSa@users.noreply.github.com>
2020-05-22 21:32:59 -0300

    model

---
+++ model/Usuario.php
@@ -0,0 +1,43 @@
+<?php
+
+class Usuario {
+
+    function __construct(){}
+
+    public $nome;
+    public $email;
+    public $confirmarEmail;
+    public $senha;
+    public $confirmarSenha;
+    public $dataNascimento;
+    public $sexo;
+    public $telefone;
+    public $celular;
+    public $endereco;
+    public $mensagem;
+
+
+    public function montaUsuario($nome,$email,$senha,$dataNascimento,$sexo,$telefone,$celular,$endereco,$mensagem){
+
+         $usuarioMontado = array($nome + $email + $senha + $dataNascimento + $sexo + $telefone + $celular + $endereco + $mensagem);
+
+         return $usuarioMontado;
+    }
+
+    public function valida($email, $confirmarEmail, $senha, $confirmarSenha){
+
+        if($email == $confirmarEmail){
+
+            return $email;
+
+        }else if ($senha == $confirmarSenha){
+
+            return $senha;
+        }else {
+            $email = null;
+            $senha = null;
+        }
+    }
+
+};
+?>
