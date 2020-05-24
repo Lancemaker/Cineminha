6a5ef191a8356a6a33173cfcd7c2d91954b8616e
LeonardoSa <33468255+LeoonardoSa@users.noreply.github.com>
2020-05-24 17:46:35 -0300

    Ajustes Produto

--- model/Produto.php
+++ model/Produto.php
@@ -1,21 +1,18 @@
 <?php 
     Class Produto{
-        private $NomeProduto
-        private $PrecoProduto 
-        private $DescricaoProduto
+        private $NomeProduto;
+        private $PrecoProduto;
+        private $DescricaoProduto;
 
 
         public function todos(){
             $data = $this->dados;
             return $data;
-
         }
 
         public function ver(){
-            $data['registro'] = this->dados[$_GET['id']];
+            $data['registro'] = $this->dados[$_GET['id']];
             return $data; 
-
         }
-
     }
         ?>
