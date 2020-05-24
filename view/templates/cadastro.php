911a0f50baebbb19a1f7b42c9ab9b87fc55dcc42
LeonardoSa <33468255+LeoonardoSa@users.noreply.github.com>
2020-05-24 18:48:18 -0300

    Produtos

--- view/templates/cadastro.php
+++ view/templates/cadastro.php
@@ -1,21 +1,23 @@
 <div class="container-cadastro">
-    <fieldset class="formulario" style="width: 30%; margin:0 auto">
+    <fieldset class="formulario">
         <div class="formulario-interno">
-            <h1 class=title-cadastro>Cadastro de Produtos</h1>
-            <label class="label-cadastro" for="nome produto">Produto<br>
-                <input class="input-text" type="text" name="nmProduto" placeholder="Picoca" width="50%" />
-            </label>
+            <h1 class=title-cadastro style="width: 42%; margin:0 auto">Cadastro de Produtos/Serviços</h1>
             <br>
-            <label class="label-cadastro" for="valor produto">Valor - R$<br>
-                <input class="input-text" type="text" name="vrProduto" placeholder="R$ 0,00" width="50%" />
-            </label>
-            <br>
-            <label class="label-cadastro" for="valor produto">Descrição produto<br>
-                <input class="input-text" type="text" name="dsProduto" placeholder="pipoca com manteiga..." width="50%" />
-            </label>
-            <br>
-            
-             <input type="button" id="signin" class="btn-login btn-second cadastro" style="margin-top: 25px; margin-left: 91px" onclick="location.href='http://localhost/index.php?page=main';" value="Cadastrar" />
+            <div style="width: 30%; margin:0 auto">
+                <label class="label-cadastro" for="nome produto" style="margin-left: 30%">Produto<br>
+                </label>
+            </div>
+
+            <img src="assets/thumbs/dbox.jpg" alt="dbox" width="350" style="display:block; margin-left: auto; margin-right: auto;">
+            <input type="checkbox" id="dbox" name="DBOX" style="margin-left: 37%">
+            <label for="DBOX">DBOX</label>
+
+            <img src="assets/thumbs/xd.png" alt="xd" width="350" style="display:block; margin-left: auto; margin-right: auto;">
+
+            <img src="assets/thumbs/pipoca.jpg" alt="pipoca" width="350" style="display:block; margin-left: auto; margin-right: auto;">
+
+
+            <input type="button" id="signin" class="btn-login btn-second cadastro" style="margin-top: 25px; margin-left: 91px" onclick="location.href='http://localhost/index.php?page=main';" value="Cadastrar" />
         </div>
         <script src="jquery.js" type="text/javascript"></script>
         <script src="jquery.maskedinput.js" type="text/javascript"></script>
