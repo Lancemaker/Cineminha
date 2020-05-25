9c173b3477845e67c0b27a336b8f68ab8cfb9d7f
FeSeiji <Felipe280699>
2020-05-24 23:06:04 -0300

    Mais produtos + botao de excluir

---
+++ view/templates/cadastro.php
@@ -0,0 +1,59 @@
+<div class="container-cadastro">
+
+    <div class="formulario-interno">
+        <h1 class=title-cadastro style="width: 42%; margin:0 auto">Cadastro de Produtos/Serviços</h1>
+        <br>
+        <div style="width: 30%; margin:0 auto">
+            <label class="label-cadastro" for="nome produto" style="text-align:center">Produtos/Serviços<br>
+            </label>
+        </div>
+        <div class="jumbotron p-4 p-md-5 text-white rounded bg-dark">
+            <form action="" method="post">
+
+                <div>
+                    <img src="assets/thumbs/dbox.jpg" alt="dbox" width="350" style="display:block; margin-left: auto; margin-right: auto;">
+                    <input type="checkbox" id="dbox" name="dbox" style="margin-left: 37%">
+                    <label for="dbox">DBOX</label>
+                </div>
+
+                <div>
+                    <img src="assets/thumbs/xd.png" alt="xd" width="350" style="display:block; margin-left: auto; margin-right: auto;">
+                    <input type="checkbox" id="xd" name="xd" style="margin-left: 37%">
+                    <label for="xd">XD</label>
+                </div>
+
+                <div>
+                    <img src="assets/thumbs/pipoca.jpg" alt="pipoca" width="350" style="display:block; margin-left: auto; margin-right: auto;">
+                    <input type="checkbox" id="pipoca" name="pipoca" style="margin-left: 37%">
+                    <label for="pipoca">Pipoca</label>
+                </div>
+
+                
+                <div>
+                    <img src="assets/thumbs/refri.jpeg" alt="refrigerante" width="350" style="display:block; margin-left: auto; margin-right: auto;">
+                    <input type="checkbox" id="refrigerante" name="refrigerante" style="margin-left: 37%">
+                    <label for="refrigerante">Refrigerante</label>
+                </div>
+
+                
+                <div>
+                    <img src="assets/thumbs/M&Ms.jpg" alt="Doces" width="350" style="display:block; margin-left: auto; margin-right: auto;">
+                    <input type="checkbox" id="doce" name="Doces" style="margin-left: 37%">
+                    <label for="Doces">Doces</label>
+                </div>
+
+                <div style="width: 150px; margin:0 auto; height: 8%">
+                    <input type="submit" id="signin" class="btn-login btn-produto title-cadastro" style="margin-top: 25px;" value="Cadastrar" />
+                    
+                </div>
+                <div style="width: 150px; margin:0 auto; height: 8%">
+                    <input type="input" id="signin" class="btn-login btn-produto title-cadastro" style="margin-top: 25px;" value="Excluir" />
+                    
+                </div>
+            </form>
+
+        </div>
+
+    </div>
+    <script src="jquery.js" type="text/javascript"></script>
+    <script src="jquery.maskedinput.js" type="text/javascript"></script>
