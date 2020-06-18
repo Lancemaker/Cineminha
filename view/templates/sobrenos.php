42501528c1b12ba2ff0cfd0c5049dd27bd095bbb
Gil <lancemaker@gmail.com>
2020-06-18 01:09:15 -0300

    nunca acaba ?

--- view/templates/sobrenos.php
+++ view/templates/sobrenos.php
@@ -6,7 +6,7 @@
                 <h4 class="mb-0">Gil Felipe da Silva</h4>
                 <strong class="d-inline-block mb-2 text-primary">Analista Júnior @ Infinity</strong>
                 <p class="card-text mb-auto">sabe dar print screen com o mouse</p>
-                <a href="index.php?page=CVGil" class="stretched-link">Seguir para currículo</a>
+                <a href="index.php?page=/templates/CVGil" class="stretched-link">Seguir para currículo</a>
             </div>
             <div class="col-auto d-none d-lg-block">
                 <img src="../assets/thumbs/Gil.jpg" alt="Gil">
@@ -20,7 +20,7 @@
                 <h4 class="mb-0">Rodrigo Torres Savone</h4>
                 <strong class="d-inline-block mb-2 text-primary">Analista Júnior @ 7comm</strong>
                 <p class="card-text mb-auto">sabe dar print screen com o mouse</p>
-                <a href="index.php?page=CVRodrigo" class="stretched-link">Seguir para currículo</a>
+                <a href="index.php?page=/templates/CVRodrigo" class="stretched-link">Seguir para currículo</a>
             </div>
             <div class="col-auto d-none d-lg-block">
                 <img src="../assets/thumbs/Rodrigo.jpg" alt="Rodrigo">
@@ -34,7 +34,7 @@
                 <h4 class="mb-0">Leonardo de Sá Santos</h4>
                 <strong class="d-inline-block mb-2 text-primary">Treinee @ 7comm</strong>
                 <p class="card-text mb-auto">sabe dar print screen com o mouse</p>
-                <a href="index.php?page=CVLeo" class="stretched-link">Seguir para currículo</a>
+                <a href="index.php?page=/templates/CVLeo" class="stretched-link">Seguir para currículo</a>
             </div>
             <div class="col-auto d-none d-lg-block">
                 <img src="../assets/thumbs/Leo.jpg" alt="Leo">
@@ -48,7 +48,7 @@
                 <h4 class="mb-0">Felipe Seiji</h4>
                 <strong class="d-inline-block mb-2 text-primary">Analista Júnior @ Infinity</strong>
                 <p class="card-text mb-auto">sabe dar print screen com o mouse</p>
-                <a href="index.php?page=CVSeiji" class="stretched-link">Seguir para currículo</a>
+                <a href="index.php?page=/templates/CVSeiji" class="stretched-link">Seguir para currículo</a>
             </div>
             <div class="col-auto d-none d-lg-block">
                 <img src="../assets/thumbs/Seiji.jpg" alt="Seiji">
@@ -62,7 +62,7 @@
                 <h4 class="mb-0">Makaya Afonso</h4>
                 <strong class="d-inline-block mb-2 text-primary">Desenvolvedor Júnior</strong>
                 <p class="card-text mb-auto">sabe dar print screen com o mouse</p>
-                <a href="index.php?page=CVMakaya" class="stretched-link">Seguir para currículo</a>
+                <a href="index.php?page=/templates/CVMakaya" class="stretched-link">Seguir para currículo</a>
             </div>
             <div class="col-auto d-none d-lg-block">
                 <img width='199px' height='199px' src="../assets/thumbs/makaya.jpeg" alt="makaya">
